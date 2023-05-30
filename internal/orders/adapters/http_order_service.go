079a508cec0eee121fdd51abafeb2cb1564fa0bf
Ben Sivoravong <ben@spacee.com>
2023-05-30 13:08:17 -0500

    Switch from basic http package to gin

--- internal/orders/adapters/http_order_service.go
+++ internal/orders/adapters/http_order_service.go
@@ -3,10 +3,11 @@ package adapters
 import (
 	"encoding/json"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 
 	"github.com/bensivo/hexagonal-architecture-study/internal/orders"
 )
@@ -23,75 +24,65 @@ func NewHttpAdapter(svc orders.OrderService) *HttpOrderService {
 }
 
 func (hos *HttpOrderService) Start() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/orders", func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case http.MethodGet:
-			res, err := hos.svc.GetOrders()
-			if err != nil {
-				fmt.Printf("Error getting orders: %v\n", err)
-				http.Error(w, "Unknown error", http.StatusInternalServerError)
-				return
-			}
-
-			resJson, err := json.Marshal(res)
-			if err != nil {
-				fmt.Printf("Error marshalling orders: %v\n", err)
-				http.Error(w, "Unknown error", http.StatusInternalServerError)
-				return
-			}
-
-			_, err = io.WriteString(w, string(resJson))
-			if err != nil {
-				fmt.Printf("Error writing response: %v\n", err)
-				return
-			}
+	r := gin.Default()
+	r.GET("/orders", func(c *gin.Context) {
+		res, err := hos.svc.GetOrders()
+		if err != nil {
+			fmt.Printf("Error getting orders: %v\n", err)
+			http.Error(c.Writer, "Unknown error", http.StatusInternalServerError)
 			return
-		case http.MethodPost:
-			body, err := ioutil.ReadAll(r.Body)
-			if err != nil {
-				fmt.Printf("Error reading body: %v\n", err)
-				http.Error(w, "Failed to read body", http.StatusBadRequest)
-				return
-			}
-
-			dto := &struct {
-				Product  string
-				Quantity int
-			}{}
-			err = json.Unmarshal(body, dto)
-			if err != nil {
-				fmt.Printf("Error unmarshalling JSON: %v\n", err)
-				http.Error(w, "Failed to parse JSON", http.StatusBadRequest)
-				return
-			}
-
-			fmt.Printf("Creating order with product: %s, quantity: %d\n", dto.Product, dto.Quantity)
-			order, err := hos.svc.CreateOrder(dto.Product, dto.Quantity)
-			if err != nil {
-				fmt.Printf("Error creating order: %v\n", err)
-				http.Error(w, "Failed to create order", http.StatusBadRequest)
-				return
-			}
-
-			fmt.Printf("Successfully created order: %s\n", order.ID)
-			resJson, err := json.Marshal(order)
-			if err != nil {
-				fmt.Printf("Error marshalling order: %v\n", err)
-				http.Error(w, "Unknown error", http.StatusInternalServerError)
-				return
-			}
-
-			io.WriteString(w, string(resJson))
-			return
-		default:
-			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		}
+
+		c.JSON(200, res)
+	})
+
+	r.GET("/orders/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		res, err := hos.svc.GetOrder(id)
+		if err != nil {
+			fmt.Printf("Error getting order %s: %v\n", id, err)
+			http.Error(c.Writer, "Unknown error", http.StatusInternalServerError)
+			return
+		}
+
+		c.JSON(200, res)
+	})
+
+	r.POST("/orders", func(c *gin.Context) {
+		body, err := ioutil.ReadAll(c.Request.Body)
+		if err != nil {
+			fmt.Printf("Error reading body: %v\n", err)
+			http.Error(c.Writer, "Failed to read body", http.StatusBadRequest)
+			return
+		}
+
+		dto := &struct {
+			Product  string
+			Quantity int
+		}{}
+		err = json.Unmarshal(body, dto)
+		if err != nil {
+			fmt.Printf("Error unmarshalling JSON: %v\n", err)
+			http.Error(c.Writer, "Failed to parse JSON", http.StatusBadRequest)
+			return
+		}
+
+		fmt.Printf("Creating order with product: %s, quantity: %d\n", dto.Product, dto.Quantity)
+		order, err := hos.svc.CreateOrder(dto.Product, dto.Quantity)
+		if err != nil {
+			fmt.Printf("Error creating order: %v\n", err)
+			http.Error(c.Writer, "Failed to create order", http.StatusBadRequest)
+			return
+		}
+
+		fmt.Printf("Successfully created order: %s\n", order.ID)
+		c.JSON(200, order)
 	})
 
 	fmt.Println("Listening at http://localhost:9999")
-	err := http.ListenAndServe(":9999", mux)
+	err := r.Run(":9999")
 	if err != nil {
 		log.Panicf("Error creating HTTP service: %v\n", err)
 	}
+
 }
